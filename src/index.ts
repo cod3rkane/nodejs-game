f0005a87727811d6ee7c816a67bb7209ae863b25
Cod3rkane <jcesar.developer@gmail.com>
2021-11-26 23:41:49 -0500

    main game loop

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,30 @@
 import './style.css';
 
-function main() {
+let ctx: CanvasRenderingContext2D;
+
+function createCanvas() {
   const canvas = document.createElement('canvas');
   canvas.setAttribute('id', 'canvas');
   canvas.setAttribute('width', '100%');
   canvas.setAttribute('height', '100%');
 
-  const ctx = canvas.getContext('2d');
-
-  ctx.fillStyle = 'green';
-  ctx.fillRect(0, 0, 100, 100);
-
-  // Lodash, currently included via a script, is required for this line to work
-  canvas.innerHTML = ['Hello', 'webpack'].join(' ');
-
   return canvas;
 }
 
-document.body.appendChild(main());
+// Main Game Loop
+function main() {
+  window.requestAnimationFrame(main);
+
+  ctx.fillStyle = 'green';
+  ctx.fillRect(0, 0, 100, 100);
+}
+
+function init() {
+  const canvas = createCanvas();
+  ctx = canvas.getContext('2d');
+
+  document.body.appendChild(canvas);
+  window.requestAnimationFrame(main);
+}
+
+window.onload = init;
